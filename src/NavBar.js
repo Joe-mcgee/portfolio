f5abe30424fe0b7131f5406cc5d3bd59af1ad545
joe-mcgee <thulsmans.2994@me.com>
2018-03-25 21:24:46 -0600

    init

---
+++ src/NavBar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+
+  render() {
+    return (
+      <div className='NavBar'>
+      </div>
+      );
+  }
+}
+
+export default NavBar;
