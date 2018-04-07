36e67752da322073e6f2147572ce7c1f63bdec06
Taylor <thulsmans.2994@me.com>
2018-04-07 17:08:26 -0600

    need api server

--- src/Blog/BlogList/BlogList.js
+++ src/Blog/BlogList/BlogList.js
@@ -10,6 +10,20 @@ class BlogList extends Component {
     }
   }
 
+
+
+
+  pullContent = async () => {
+    const json = await fetch('https://thulsmans2994.wordpress.com/');
+    const response = await JSON.parse(json);
+    console.log(response)
+    return response
+  }
+
+  componentDidMount = () => {
+    this.pullContent();
+  }
+
   render () {
 
     return (<div className='BlogList'>
